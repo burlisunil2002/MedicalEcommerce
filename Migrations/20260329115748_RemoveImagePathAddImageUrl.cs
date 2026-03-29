3760421097715ddb62d962a0ed0823d9bbe5fb45
unknown <burlisunil16@gmail.com>
2026-03-29 17:31:00 +0530

    removed bin and obj

---
+++ Migrations/20260329115748_RemoveImagePathAddImageUrl.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VivekMedicalProducts.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveImagePathAddImageUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
