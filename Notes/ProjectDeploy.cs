b09c67953dd3a6fb529f88964aa6c3f5737083d9
unknown <burlisunil16@gmail.com>
2026-07-13 10:47:25 +0530

    GANESHA SAVE ME 8

--- Notes/ProjectDeploy.cs
+++ Notes/ProjectDeploy.cs
@@ -4,63 +4,63 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Org.BouncyCastle.Asn1.X509;
 
 
-  /* <Target Name = "ReactBuild" BeforeTargets="Build">
+/* <Target Name = "ReactBuild" BeforeTargets="Build">
 
-		<Message Text = "Building React..." Importance="high" />
+      <Message Text = "Building React..." Importance="high" />
 
-		<Exec WorkingDirectory = "$(MSBuildProjectDirectory)\sunilmedical-ui" Command="npm install" Condition="!Exists('$(MSBuildProjectDirectory)\sunilmedical-ui\node_modules')" />
+      <Exec WorkingDirectory = "$(MSBuildProjectDirectory)\sunilmedical-ui" Command="npm install" Condition="!Exists('$(MSBuildProjectDirectory)\sunilmedical-ui\node_modules')" />
 
-		<Exec WorkingDirectory = "$(MSBuildProjectDirectory)\sunilmedical-ui" Command="npm run build" />
+      <Exec WorkingDirectory = "$(MSBuildProjectDirectory)\sunilmedical-ui" Command="npm run build" />
 
-		<RemoveDir Directories = "$(ProjectDir)wwwroot\react" ContinueOnError="true" />
+      <RemoveDir Directories = "$(ProjectDir)wwwroot\react" ContinueOnError="true" />
 
-		<MakeDir Directories = "$(ProjectDir)wwwroot\react" />
-
-
-        < ItemGroup >
-
-            < ReactFiles Include="$(ProjectDir)sunilmedical-ui\build\**\*" />
-
-		</ItemGroup>
-
-		<Copy SourceFiles = "@(ReactFiles)" DestinationFiles="@(ReactFiles->'$(ProjectDir)wwwroot\react\%(RecursiveDir)%(Filename)%(Extension)')" SkipUnchangedFiles="true" />
-
-	</Target> */
+      <MakeDir Directories = "$(ProjectDir)wwwroot\react" />
 
 
-/* < Target Name = "PublishReact" BeforeTargets = "Publish" >
+      < ItemGroup >
 
-  < Message Text = "========== Building React Application ==========" Importance = "high" />
+          < ReactFiles Include="$(ProjectDir)sunilmedical-ui\build\**\*" />
 
-  < !--Install dependencies-- >
-  < Exec
+      </ItemGroup>
+
+      <Copy SourceFiles = "@(ReactFiles)" DestinationFiles="@(ReactFiles->'$(ProjectDir)wwwroot\react\%(RecursiveDir)%(Filename)%(Extension)')" SkipUnchangedFiles="true" />
+
+  </Target> */
+
+
+/* <Target Name = "PublishReact" BeforeTargets = "Publish" >
+
+	<Message Text = "========== Building React Application ==========" Importance = "high" />
+
+	<!--Install dependencies-->
+	<Exec
       WorkingDirectory = "$(MSBuildProjectDirectory)\sunilmedical-ui"
       Command = "npm ci"
       Condition = "Exists('$(MSBuildProjectDirectory)\sunilmedical-ui\package-lock.json')" />
 
-  < Exec
+	<Exec
       WorkingDirectory = "$(MSBuildProjectDirectory)\sunilmedical-ui"
       Command = "npm install"
       Condition = "!Exists('$(MSBuildProjectDirectory)\sunilmedical-ui\package-lock.json')" />
 
-  < !--Build React-- >
-  < Exec
+	<!--Build React-->
+	<Exec
       WorkingDirectory = "$(MSBuildProjectDirectory)\sunilmedical-ui"
       Command = "npm run build" />
 
-  < !--Create publish React folder -->
-  <MakeDir Directories="$(PublishDir)wwwroot\react" />
+	<!--Create publish React folder -->
+	<MakeDir Directories="$(PublishDir)wwwroot\react" />
 
-  <!-- Collect React build files -->
-  <ItemGroup>
-    <ReactBuildFiles Include="$(MSBuildProjectDirectory)\sunilmedical-ui\build\**\*" />
-  </ItemGroup>
+	<!-- Collect React build files -->
+	<ItemGroup>
+		<ReactBuildFiles Include="$(MSBuildProjectDirectory)\sunilmedical-ui\build\**\*" />
+	</ItemGroup>
 
-  <!-- Copy React build into publish folder -->
-  <Copy
-      SourceFiles="@(ReactBuildFiles)"
-      DestinationFiles="@(ReactBuildFiles->'$(PublishDir)wwwroot\react\%(RecursiveDir)%(Filename)%(Extension)')"
-      SkipUnchangedFiles="true" />
+	<!-- Copy React build into publish folder -->
+	<Copy
+		SourceFiles="@(ReactBuildFiles)"
+		DestinationFiles="@(ReactBuildFiles->'$(PublishDir)wwwroot\react\%(RecursiveDir)%(Filename)%(Extension)')"
+		SkipUnchangedFiles="true" />
 
-</Target> */
+	</Target> */
 
